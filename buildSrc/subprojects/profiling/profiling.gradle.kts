1bc59dcba1aac8dce5ddb614d3058140524d1b0f
Rajagopalan Nanjundan <Perundurai-Products@users.noreply.github.com>
2022-10-28 16:29:19 +0530

    Initialize cc/dm/insight/fix-duplicates

---
+++ buildSrc/subprojects/profiling/profiling.gradle.kts
@@ -0,0 +1,27 @@
+import org.gradle.kotlin.dsl.plugins.precompiled.PrecompiledScriptPlugins
+
+plugins {
+    `java-gradle-plugin`
+}
+
+apply {
+    plugin("org.gradle.kotlin.kotlin-dsl")
+    plugin<PrecompiledScriptPlugins>()
+}
+
+dependencies {
+    implementation("me.champeau.gradle:jmh-gradle-plugin:0.4.6")
+    implementation("org.jsoup:jsoup:1.11.2")
+    implementation("com.gradle:build-scan-plugin:1.13.4")
+    implementation(project(":configuration"))
+    implementation(project(":kotlinDsl"))
+}
+
+gradlePlugin {
+    (plugins) {
+        "buildscan" {
+            id = "gradlebuild.buildscan"
+            implementationClass = "org.gradle.gradlebuild.profiling.buildscan.BuildScanPlugin"
+        }
+    }
+}

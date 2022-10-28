fb97c948b754d47c33eb957bc9030c315d36b317
Rajagopalan Nanjundan <Perundurai-Products@users.noreply.github.com>
2022-10-28 18:16:37 +0530

    Initialize wolfs/extract-property-discovery-review

---
+++ buildSrc/subprojects/uber-plugins/uber-plugins.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    implementation(project(":binaryCompatibility"))
+    implementation(project(":cleanup"))
+    implementation(project(":configuration"))
+    implementation(project(":kotlinDsl"))
+    implementation(project(":profiling"))
+    implementation(project(":plugins"))
+}

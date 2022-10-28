cf3c590e59a851631e99f12020c5b745b3865f7e
Rajagopalan Nanjundan <Perundurai-Products@users.noreply.github.com>
2022-10-28 17:29:24 +0530

    Initialize sg/remove-logging

---
+++ buildSrc/subprojects/profiling/profiling.gradle.kts
@@ -0,0 +1,16 @@
+dependencies {
+    implementation("me.champeau.gradle:jmh-gradle-plugin:0.4.7")
+    implementation("org.jsoup:jsoup:1.11.3")
+    implementation("com.gradle:build-scan-plugin:2.0")
+    implementation(project(":configuration"))
+    implementation(project(":kotlinDsl"))
+}
+
+gradlePlugin {
+    plugins {
+        register("buildscan") {
+            id = "gradlebuild.buildscan"
+            implementationClass = "org.gradle.gradlebuild.profiling.buildscan.BuildScanPlugin"
+        }
+    }
+}

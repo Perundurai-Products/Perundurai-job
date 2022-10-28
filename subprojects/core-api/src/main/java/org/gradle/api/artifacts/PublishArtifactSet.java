dc7d1678b0eaf5984a1fbac8aa3740c0695512d3
Rajagopalan Nanjundan <Perundurai-Products@users.noreply.github.com>
2022-10-28 18:06:15 +0530

    Initialize sg/lazy/publish-register

---
+++ subprojects/core-api/src/main/java/org/gradle/api/artifacts/PublishArtifactSet.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gradle.api.artifacts;
+
+import org.gradle.api.Action;
+import org.gradle.api.Buildable;
+import org.gradle.api.DomainObjectProvider;
+import org.gradle.api.DomainObjectSet;
+import org.gradle.api.Incubating;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.FileSystemLocation;
+import org.gradle.api.provider.Provider;
+
+/**
+ * A set of artifacts to be published.
+ */
+public interface PublishArtifactSet extends DomainObjectSet<PublishArtifact>, Buildable {
+    /**
+     * Registers a file for publication.
+     *
+     * @param artifactFile provider to file to register
+     * @param configuration action to configure a publish artifact
+     * @return a Provider to the publish artifact
+     * @since 5.1
+     */
+    @Incubating
+    DomainObjectProvider<PublishArtifact> register(Provider<? extends FileSystemLocation> artifactFile, Action<? super ConfigurablePublishArtifact> configuration);
+
+    FileCollection getFiles();
+}
---
+++ subprojects/core/src/main/java/org/gradle/api/internal/artifacts/publish/DefaultPublishArtifact.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.internal.artifacts.publish;
+
+import org.gradle.api.artifacts.ConfigurablePublishArtifact;
+import org.gradle.api.internal.tasks.TaskResolver;
+
+import java.io.File;
+import java.util.Date;
+import java.util.Objects;
+
+public class DefaultPublishArtifact extends AbstractPublishArtifact implements ConfigurablePublishArtifact {
+    private String name;
+    private String extension;
+    private String type;
+    private String classifier;
+    private Date date;
+    private File file;
+
+    public DefaultPublishArtifact(TaskResolver resolver,
+                                  String name, String extension, String type,
+                                  String classifier, Date date, File file, Object... tasks) {
+        super(resolver, tasks);
+        this.name = name;
+        this.extension = extension;
+        this.type = type;
+        this.date = date;
+        this.classifier = classifier;
+        this.file = file;
+    }
+
+    public DefaultPublishArtifact(String name, String extension, String type,
+                                  String classifier, Date date, File file, Object... tasks) {
+        super(tasks);
+        this.name = name;
+        this.extension = extension;
+        this.type = type;
+        this.date = date;
+        this.classifier = classifier;
+        this.file = file;
+    }
+
+    @Override
+    public DefaultPublishArtifact builtBy(Object... tasks) {
+        super.builtBy(tasks);
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getClassifier() {
+        return classifier;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setClassifier(String classifier) {
+        this.classifier = classifier;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        DefaultPublishArtifact that = (DefaultPublishArtifact) o;
+        return getName().equals(that.getName()) &&
+                getExtension().equals(that.getExtension()) &&
+                getType().equals(that.getType()) &&
+                Objects.equals(getClassifier(), that.getClassifier()) &&
+                getFile().equals(that.getFile());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getExtension(), getType(), getClassifier(), getFile());
+    }
+}

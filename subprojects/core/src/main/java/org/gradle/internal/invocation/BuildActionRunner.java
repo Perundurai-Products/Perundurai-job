a7af5886eb6f81876f65d538010ccf2050cc862c
Rajagopalan Nanjundan <Perundurai-Products@users.noreply.github.com>
2022-10-28 17:40:34 +0530

    Initialize wolfs/xforms/execution-history-review

---
+++ subprojects/core/src/main/java/org/gradle/internal/invocation/BuildActionRunner.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.internal.invocation;
+
+import javax.annotation.Nullable;
+
+/**
+ * Responsible for executing a {@link BuildAction} and generating the result.
+ */
+public interface BuildActionRunner {
+    /**
+     * Runs the given action, returning a result that describes the build outcome and the result that should be returned to the client.
+     *
+     * <p>Build failures should be packaged in the returned result, rather than thrown.
+     */
+    Result run(BuildAction action, BuildController buildController);
+
+    class Result {
+        private final boolean hasResult;
+        private final Object result;
+        private final Throwable buildFailure;
+        private final Throwable clientFailure;
+        private static final Result NOTHING = new Result(false, null, null, null);
+
+        private Result(boolean hasResult, Object result, Throwable buildFailure, Throwable clientFailure) {
+            this.hasResult = hasResult;
+            this.result = result;
+            this.buildFailure = buildFailure;
+            this.clientFailure = clientFailure;
+        }
+
+        public static Result nothing() {
+            return NOTHING;
+        }
+
+        public static Result of(@Nullable Object result) {
+            return new Result(true, result, null, null);
+        }
+
+        public static Result of(@Nullable Object result, Throwable buildFailure) {
+            return new Result(true, result, buildFailure, null);
+        }
+
+        public static Result failed(Throwable buildFailure) {
+            return new Result(true, null, buildFailure, buildFailure);
+        }
+
+        public static Result failed(Throwable buildFailure, RuntimeException clientFailure) {
+            return new Result(true, null, buildFailure, clientFailure);
+        }
+
+        /**
+         * Replaces the client exception.
+         */
+        public Result withClientFailure(Throwable clientFailure) {
+            return new Result(hasResult, result, buildFailure, clientFailure);
+        }
+
+        /**
+         * Is there a result, possibly a null object or a build failure?
+         */
+        public boolean hasResult() {
+            return hasResult;
+        }
+
+        /**
+         * Returns the result to send back to the client, possibly null.
+         */
+        @Nullable
+        public Object getClientResult() {
+            return result;
+        }
+
+        /**
+         * Returns the failure to report in the build outcome.
+         */
+        @Nullable
+        public Throwable getBuildFailure() {
+            return buildFailure;
+        }
+
+        /**
+         * Returns the failure to send back to the client.
+         */
+        @Nullable
+        public Throwable getClientFailure() {
+            return clientFailure;
+        }
+
+    }
+}
---
+++ subprojects/dependency-management/src/main/java/org/gradle/api/internal/artifacts/DependencyManagementGradleUserHomeScopeServices.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.internal.artifacts;
+
+import org.gradle.api.internal.artifacts.ivyservice.ArtifactCacheLockingManager;
+import org.gradle.api.internal.artifacts.ivyservice.ArtifactCacheMetadata;
+import org.gradle.api.internal.artifacts.ivyservice.DefaultArtifactCacheLockingManager;
+import org.gradle.api.internal.artifacts.ivyservice.DefaultArtifactCacheMetadata;
+import org.gradle.api.internal.artifacts.transform.DefaultTransformerExecutionHistoryRepository;
+import org.gradle.api.internal.artifacts.transform.GradleUserHomeWorkspaceProvider;
+import org.gradle.api.internal.artifacts.transform.TransformerExecutionHistoryRepository;
+import org.gradle.api.internal.artifacts.transform.TransformerWorkspaceProvider;
+import org.gradle.api.internal.cache.StringInterner;
+import org.gradle.api.internal.changedetection.state.DefaultExecutionHistoryCacheAccess;
+import org.gradle.cache.CacheRepository;
+import org.gradle.cache.internal.CacheScopeMapping;
+import org.gradle.cache.internal.InMemoryCacheDecoratorFactory;
+import org.gradle.cache.internal.UsedGradleVersions;
+import org.gradle.internal.execution.history.ExecutionHistoryCacheAccess;
+import org.gradle.internal.execution.history.ExecutionHistoryStore;
+import org.gradle.internal.execution.history.impl.DefaultExecutionHistoryStore;
+import org.gradle.internal.resource.local.FileAccessTimeJournal;
+
+public class DependencyManagementGradleUserHomeScopeServices {
+    DefaultArtifactCacheMetadata createArtifactCacheMetaData(CacheScopeMapping cacheScopeMapping) {
+        return new DefaultArtifactCacheMetadata(cacheScopeMapping);
+    }
+
+    ArtifactCacheLockingManager createArtifactCacheLockingManager(CacheRepository cacheRepository, ArtifactCacheMetadata artifactCacheMetadata, FileAccessTimeJournal fileAccessTimeJournal,
+                                                                  UsedGradleVersions usedGradleVersions) {
+        return new DefaultArtifactCacheLockingManager(cacheRepository, artifactCacheMetadata, fileAccessTimeJournal, usedGradleVersions);
+    }
+
+    ExecutionHistoryCacheAccess createExecutionHistoryCacheAccess(CacheRepository cacheRepository, InMemoryCacheDecoratorFactory inMemoryCacheDecoratorFactory) {
+        return new DefaultExecutionHistoryCacheAccess(null, cacheRepository, inMemoryCacheDecoratorFactory);
+    }
+
+    ExecutionHistoryStore createExecutionHistoryStore(ExecutionHistoryCacheAccess executionHistoryCacheAccess, StringInterner stringInterner) {
+        return new DefaultExecutionHistoryStore(executionHistoryCacheAccess, stringInterner);
+    }
+
+    TransformerWorkspaceProvider createTransformerWorkspaceProvider(ArtifactCacheMetadata artifactCacheMetadata, CacheRepository cacheRepository, FileAccessTimeJournal fileAccessTimeJournal) {
+        return new GradleUserHomeWorkspaceProvider(artifactCacheMetadata.getTransformsStoreDirectory(), cacheRepository, fileAccessTimeJournal);
+    }
+
+    TransformerExecutionHistoryRepository createTransformerExecutionHistoryRepository(TransformerWorkspaceProvider transformerWorkspaceProvider, ExecutionHistoryStore executionHistoryStore) {
+        return new DefaultTransformerExecutionHistoryRepository(transformerWorkspaceProvider, executionHistoryStore);
+    }
+}
